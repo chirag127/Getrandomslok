79f8280197253d0235a128bf362825c3d7efb578
Chirag <whyiswhen@gmail.com>
2021-10-01 22:33:13 +0530

    added meaning

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 fetch("https://bhagavadgitaapi.in/slok?api_key=b89a6a08cd62a233e")
+//fetch("https://bhagavadgitaapi.in/slok", {headers: {'x-api-key': 'b89a6a08cd62a233e'}})
+
   .then(data => data.json())
   .then(sData => {
-      const sText = sData.slok;
-      const sp = document.getElementById('sp')
+      const slokT = sData.slok;
+      const siva = sData.siva.et;
 
-      sp.innerHTML = sText;
-  })
+      const sp = document.getElementById('sp')
+      const mp = document.getElementById('mp')
+
+      sp.innerHTML = slokT  ;
+      mp.innerHTML = "It's chapter number.verse number and meaning are :- "+ siva  ;
+
+  }) 
